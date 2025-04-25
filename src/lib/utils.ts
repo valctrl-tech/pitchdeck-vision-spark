4b22dc75b633bde4c416e1a3fa021f49c2a43d1d
valctrl-tech <tech@valctrl.com>
2025-04-24 23:18:19 -0400

    Add documentation comment to utils function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,7 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
+// Utility function for combining Tailwind CSS classes
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
